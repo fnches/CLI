17a1ed33fc0fb60f18b2c34896bea35abc3c6c27
fnches <qoctopi@gmail.com>
2023-05-16 22:46:32 +0930

    Posts on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
-	async function subscribe(event: Event) {
-		const form = event.target as HTMLFormElement;
-		const data = new FormData(form);
+	import type { Post } from '@prisma/client';
 
-		await fetch('/api/newsletter', {
-			method: 'POST',
-			body: data
-		});
+	async function getPosts(params: type) {
+		const response = await fetch('api/posts');
+		const posts: Post[] = await response.json();
+		return posts;
 	}
 </script>
 
-<h1>Newsletter</h1>
+<h1>Posts</h1>
 
-<form on:submit|preventDefault={subscribe}>
-	<input type="email" name="email" />
-	<button>subscribe</button>
-</form>
+{#await getPosts()}
+	<p>Loading...</p>
+{:then posts}
+	<p>Showing {posts.length} posts.</p>
+
+	{#each posts as { slug, title }}
+		<ul><li><a href="/posts/{slug}">{title}</a></li></ul>
+	{/each}
+{:catch error}
+	<p>{error.message}</p>
+{/await}
