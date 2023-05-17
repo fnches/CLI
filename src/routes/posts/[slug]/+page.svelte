eff16520edfe53d9c609fa4dac5b7add84d2a4e4
fnches <qoctopi@gmail.com>
2023-05-17 22:54:04 +0930

    (2:17:00)

---
+++ src/routes/posts/[slug]/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { PageData } from './../../../../.svelte-kit/types/src/routes/Posts/[slug=slug]/$types.d.ts';
+
+import type {PageData}
+export let data: PageData
+
+</script>
