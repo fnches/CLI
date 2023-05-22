eaf5d7a716c62fe2f4cdab913c7b1f500315d8d9
fnches <qoctopi@gmail.com>
2023-05-22 21:51:20 +0930

    Viteconfig pollyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import inject from '@rollup/plugin-inject';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+	base: './',
+	// Node.js global to browser globalThis
+	define: { global: 'globalThis' },
+	plugins: [sveltekit(), inject({ util: 'util/' })],
+	build: {
+		rollupOptions: {
+			plugins: [nodePolyfills()]
+		},
+		commonjsOptions: { transformMixedEsModules: true }
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			// Node.js global to browser globalThis
+			define: {
+				global: 'globalThis'
+			},
+			// Enable esbuild polyfill plugins
+			plugins: [
+				NodeGlobalsPolyfillPlugin({
+					buffer: true
+				})
+			]
+		}
 	}
 });
