a66a6548f2684ec0d81a60d1d44eeb6d54c3283b
fnches <qoctopi@gmail.com>
2023-05-29 21:17:16 +0930

    viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,9 @@ export default defineConfig(() => ({
 			protocolImports: true
 		})
 	],
+
 	optimizeDeps: {
+		//these both throw a once off error if not cached in advance - https://github.com/cypress-io/cypress/issues/18124#issuecomment-1016534802
 		include: ['algosdk', '@perawallet/connect'],
 		esbuildOptions: {
 			// Node.js global to browser globalThis https://github.com/perawallet/connect/issues/101#issuecomment-1468996145
@@ -19,5 +21,8 @@ export default defineConfig(() => ({
 				'global.WebSocket': 'globalThis.WebSocket'
 			}
 		}
+	},
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
 }));
